4c4a8fd64d1f68089d1150f99b644fc4bb65dd86
Cristian1225 <cristianoreyes234@gmail.com>
2023-07-06 15:41:25 -0700

    updated index

--- script.js
+++ script.js
@@ -1,51 +1,42 @@
 function translateToPigLatin(text) {
-   
-    const words = text.split(' ');
-  
-    const translatedWords = [];
-  
-   
-    for (let i = 0; i < words.length; i++) {
-      const word = words[i];
-      const lowercaseWord = word.toLowerCase(); 
-      
-      if (word.length === 0 || word.length === 1) {
-        console.log('Unable to translate: ' + word);
-        return;
-      }
-  
-      
-      if (isVowel(lowercaseWord[0])) {
-        translatedWords.push(word + 'way');
-      }
-    
-      else if (isConsonant(lowercaseWord[0])) {
-        
-        if (isConsonant(lowercaseWord[1])) {
-          translatedWords.push(word.slice(2) + lowercaseWord.slice(0, 2) + 'ay');
-        } else {
-          translatedWords.push(word.slice(1) + lowercaseWord[0] + 'ay');
-        }
+  const words = text.split(' ');
+
+  const translatedWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const lowercaseWord = word.toLowerCase();
+
+    if (word.length === 0 || word.length === 1) {
+      console.log('Unable to translate: ' + word);
+      return;
+    }
+
+    if (isVowel(lowercaseWord[0])) {
+      translatedWords.push(word + 'way');
+    } else if (isConsonant(lowercaseWord[0])) {
+      if (lowercaseWord.length > 1 && isConsonant(lowercaseWord[1])) {
+        translatedWords.push(word.slice(2) + lowercaseWord.slice(0, 2) + 'ay');
+      } else {
+        translatedWords.push(word.slice(1) + lowercaseWord[0] + 'ay');
       }
     }
-  
-   
-    const translatedText = translatedWords.join(' ');
-  
-    
-    console.log('Translated text: ' + translatedText);
   }
-  
- 
-  function isVowel(char) {
-    return ['a', 'e', 'i', 'o', 'u'].includes(char);
-  }
-  
-  
-  function isConsonant(char) {
-    return !isVowel(char);
-  }
-  
-  // Example usage
-  const inputText = prompt('Enter a phrase:');
+
+  const translatedText = translatedWords.join(' ');
+
+  console.log('Translated text: ' + translatedText);
+}
+
+function isVowel(char) {
+  return ['a', 'e', 'i', 'o', 'u'].includes(char);
+}
+
+function isConsonant(char) {
+  return !isVowel(char);
+}
+
+const inputText = prompt('Enter a phrase:');
+if (inputText !== null) {
   translateToPigLatin(inputText);
+}
